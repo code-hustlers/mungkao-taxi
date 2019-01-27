47a3366071bb42005d6f5da89b96bdd3225c7811
Taesu Hyeon <qvil1127@gmail.com>
2019-01-27 22:53:02 +0900

    Add routing, GlobalStyles

---
+++ frontend/src/styles/GlobalStyles.js
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+  html, body, #root {
+    padding: 0;
+    margin: 0;
+    height: 100%;
+  }
+`;
+
+export default GlobalStyles;
