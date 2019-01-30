7f2f4014e20bef4650a80dbe8e42eaff9f77c855
Taesu Hyeon <qvil1127@gmail.com>
2019-01-31 02:37:14 +0900

    Add firebase with .env

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,17 @@ import GlobalStyles from "./styles/GlobalStyles";
 import AppRouter from "./Routes/AppRouter";
 import { ThemeProvider } from "styled-components";
 import DefaultTheme from "./styles/theme/DefaultTheme";
+import firebase from "firebase";
+
+const config = {
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  databaseURL: process.env.REACT_APP_databaseURL,
+  storageBucket: process.env.REACT_APP_storageBucket
+};
+firebase.initializeApp(config);
+
+console.log(config);
 
 class App extends Component {
   render() {
