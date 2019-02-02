b224c284f32c383a1a72e26361f1c8ff8fd2f5f5
Taesu Hyeon <qvil1127@gmail.com>
2019-02-02 20:49:50 +0900

    Signin API 와 UI 연동

--- frontend/src/views/Signin/Signin.js
+++ frontend/src/views/Signin/Signin.js
@@ -1,20 +1,53 @@
 import React from "react";
 import { Container, CardForm, Input, Button } from "./Signin.styles";
+import axios from "axios";
 
 class Signin extends React.Component {
-  handleSignin = event => {
-    event.preventDefault();
-    alert("Signin!");
+  state = {
+    id: "",
+    pw: ""
   };
+
+  handleSignin = event => {
+    const { id, pw } = this.state;
+    event.preventDefault();
+    axios
+      .post(
+        `${process.env.REACT_APP_SERVER_URL}:${
+          process.env.REACT_APP_SERVER_PORT
+        }/apis/v1/login`,
+        {
+          id,
+          pw
+        }
+      )
+      .then(res => console.log(res));
+  };
+
+  handleChange = name => event => {
+    this.setState({ [name]: event.target.value });
+  };
+
   render() {
-    const { handleSignin } = this;
+    const { handleSignin, handleChange } = this;
+    const { id, pw } = this.state;
+    console.log("TCL: Signin -> render -> id, pw", id, pw);
 
     return (
       <Container>
         <CardForm onSubmit={handleSignin}>
           <h1>Signin</h1>
-          <Input placeholder="Username" />
-          <Input placeholder="Password" type="password" />
+          <Input
+            value={id}
+            onChange={handleChange("id")}
+            placeholder="Username"
+          />
+          <Input
+            value={pw}
+            onChange={handleChange("pw")}
+            placeholder="Password"
+            type="password"
+          />
           <Button>Signin</Button>
         </CardForm>
       </Container>
