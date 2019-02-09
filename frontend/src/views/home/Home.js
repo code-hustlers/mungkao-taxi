e569f0ec7aa72422f8b3151351e9b410ebd19d1d
kang <kaspi6000@github.com>
2019-02-09 17:59:33 +0900

    verify home 초기화면

--- frontend/src/views/home/Home.js
+++ frontend/src/views/home/Home.js
@@ -6,10 +6,46 @@ import withStore from "../../lib/withStore";
 
 
 class Home extends React.Component {
+  state = {
+    userInfo: {}
+  };
+
+  componentDidMount() {
+    const { handleCheck } = this;
+    handleCheck();
+  }
+
+  handleCheck = async () => {
+
+    const {
+      cookies,
+      history: { push },
+      store: {
+        actions: { setSignin }
+      }
+    } = this.props;
+
+    const token = cookies.get('token');
+
+    await axios({
+      method: 'get',
+      url: `${process.env.REACT_APP_SERVER_URL}:${process.env.REACT_APP_SERVER_PORT}/apis/v1/verify?token=`+token
+    })
+    .then(res => this.setState({ userInfo: res.data }))
+    .catch(() => {
+      cookies.remove('token');
+      setSignin(false);
+    });
+  }
+
   render() {
+    const { userInfo } = this.state;
+    console.log(userInfo)
+
     return (
       <div>
-        Home
+        <h1>Home</h1>
+        <span>{userInfo.name}</span> 님, 어서오세요.
       </div>
     );
   }
