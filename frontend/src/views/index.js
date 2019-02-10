29f22656e660aec6d19f1d1c531aeb6e566c7a2b
Taesu Hyeon <qvil1127@gmail.com>
2019-02-10 18:26:50 +0900

    jsx 문법 쓰는 파일만 .jsx로 변경

--- frontend/src/views/index.js
+++ frontend/src/views/index.js
@@ -1,3 +1,3 @@
-export { default as Home } from "./home/Home";
+export { default as Home } from "./Home/Home";
 export { default as Signin } from "./Signin/Signin";
 export { default as Signup } from "./Signup/Signup";
