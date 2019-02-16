ed1c74ab2ba0dc028446bf2eddbe472658c3ec2b
Taesu Hyeon <qvil1127@gmail.com>
2019-02-17 00:08:50 +0900

    [Hotfix] Messaging error나서 일단 막음, gitignore: *.pem

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,14 +5,14 @@ import { ThemeProvider } from "styled-components";
 import DefaultTheme from "./styles/theme/DefaultTheme";
 import { CookiesProvider } from "react-cookie";
 import StoreProvider from "./store/Store";
-import Messaging from "./lib/Messaging";
+// import Messaging from "./lib/Messaging";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    const messaging = new Messaging();
-    console.log("TCL: App -> constructor -> messaging", messaging);
-  }
+  // constructor(props) {
+  //   super(props);
+  //   const messaging = new Messaging();
+  //   console.log("TCL: App -> constructor -> messaging", messaging);
+  // }
 
   render() {
     return (
