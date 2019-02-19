31b6f0c25464b62e991117d14268e1ade92fd0be
Taesu Hyeon <qvil1127@gmail.com>
2019-02-19 19:37:18 +0900

    Add /service-worker.js
    - index.js: serviceWorker.unregister() -> register()

--- frontend/public/service-worker.js
+++ frontend/public/service-worker.js
@@ -1,6 +1,8 @@
+import firebase from "firebase";
+
 importScripts("https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js");
 importScripts("https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js");
 firebase.initializeApp({
   messagingSenderId: ""
 });
-// const messaging = firebase.messaging();
+const messaging = firebase.messaging();
