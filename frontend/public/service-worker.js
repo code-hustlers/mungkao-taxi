f08412f89a9b303ee6affc6526a5a69965f76145
Taesu Hyeon <qvil1127@gmail.com>
2019-03-06 22:36:01 +0900

    PWA 적용 중

--- frontend/public/service-worker.js
+++ frontend/public/service-worker.js
@@ -1,8 +1,29 @@
 import firebase from "firebase";
 
-importScripts("https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js");
-importScripts("https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js");
-firebase.initializeApp({
-  messagingSenderId: ""
+// importScripts("https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js");
+// importScripts("https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js");
+// firebase.initializeApp({
+//   messagingSenderId: ""
+// });
+// const messaging = firebase.messaging();
+var messaging = firebase.messaging();
+
+// [START background_handler]
+messaging.setBackgroundMessageHandler(function(payload) {
+  console.log(
+    "[firebase-messaging-sw.js] Received background message ",
+    payload
+  );
+  // Customize notification here
+  var notificationTitle = "Background Message Title";
+  var notificationOptions = {
+    body: "Background Message body.",
+    icon: "/firebase-logo.png"
+  };
+
+  return self.registration.showNotification(
+    notificationTitle,
+    notificationOptions
+  );
 });
-const messaging = firebase.messaging();
+// [END background_handler]
