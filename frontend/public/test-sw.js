38881db480fb00bc83614f3cf251ef45f26de239
Taesu Hyeon <qvil1127@gmail.com>
2019-03-23 17:21:08 +0900

    Push Noti 받은 버전.(캐시 걸려서 서비스워커 파일 이름 변경해버림)

--- frontend/public/test-sw.js
+++ frontend/public/test-sw.js
@@ -1,3 +1,26 @@
 self.addEventListener("install", e => {
-  console.log("Installed My Service Worker!22222");
+  console.log("Installed My Service Worker!2333333333332222");
+});
+
+self.addEventListener("push", function(event) {
+  console.log("[Service Worker] Push Received.");
+  console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
+
+  const title = "Push Codelab";
+  const options = {
+    // body: "Yay it works.",
+    body: event.data.text(),
+    icon: "images/icon.png",
+    badge: "images/badge.png"
+  };
+
+  event.waitUntil(self.registration.showNotification(title, options));
+});
+
+self.addEventListener("notificationclick", function(event) {
+  console.log("[Service Worker] Notification click Received.");
+
+  event.notification.close();
+
+  event.waitUntil(clients.openWindow("https://developers.google.com/web/"));
 });
