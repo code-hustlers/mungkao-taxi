d237d784a8a2fa196a4741079660576fa6242a08
Taesu Hyeon <qvil1127@gmail.com>
2019-04-02 02:45:51 +0900

    setBackgroundMessageHandler 추가(push 테스트는 안되고 있음)

--- frontend/public/firebase-messaging-sw.js
+++ frontend/public/firebase-messaging-sw.js
@@ -22,3 +22,21 @@ messaging.onMessage(function(payload) {
   console.log("Message received. ", payload);
   // ...
 });
+
+messaging.setBackgroundMessageHandler(function(payload) {
+  console.log(
+    "[firebase-messaging-sw.js] Received background message ",
+    payload
+  );
+  // Customize notification here
+  var notificationTitle = "Background Message Title";
+  var notificationOptions = {
+    body: "Background Message body.",
+    icon: "/firebase-logo.png"
+  };
+
+  return self.registration.showNotification(
+    notificationTitle,
+    notificationOptions
+  );
+});
