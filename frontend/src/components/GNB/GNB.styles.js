2cddc9473d40abe78d239d6db9f89964d433ac98
KANG <kaspi6000@gmail.com>
2019-04-09 11:24:33 +0900

    헤더 포지션 앱솔루트

--- frontend/src/components/GNB/GNB.styles.js
+++ frontend/src/components/GNB/GNB.styles.js
@@ -2,9 +2,10 @@ import styled from "styled-components";
 
 export const Nav = styled.nav`
   width: 100%;
-  height: 9%;
+  // height: 9%;
   background-color: #fd79a8;
   z-index: 1;
+  position: absolute;
 `;
 
 export const Div = styled.div`
