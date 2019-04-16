923ec1aa336f0ff557918470b68104665580c17f
KANG <kaspi6000@gmail.com>
2019-04-16 20:59:54 +0900

    도착api, ui추가

--- backend/routes/callRoutes.js
+++ backend/routes/callRoutes.js
@@ -1,191 +1,266 @@
-import moment from 'moment';
-import { DEFAULT_API, USER_STATUS_READY, USER_STATUS_WORK, USER_POSITION_DRIVER, CALL_STATUS_READY, CALL_STATUS_APPROVE, CALL_STATUS_REJECT, SERVER_RESULT_SUCCESS, SERVER_RESULT_FAILURE } from '../constants';
+import moment from "moment";
+import {
+  DEFAULT_API,
+  USER_STATUS_READY,
+  USER_STATUS_WORK,
+  USER_POSITION_DRIVER,
+  CALL_STATUS_READY,
+  CALL_STATUS_APPROVE,
+  CALL_STATUS_REJECT,
+  SERVER_RESULT_SUCCESS,
+  SERVER_RESULT_FAILURE,
+  CALL_STATUS_ARRIVE
+} from "../constants";
 
 const NOW = moment().format("YYYY-MM-DD HH:mm:ss");
 
 const callRoutes = (app, User, Call) => {
+  // PUT call status
+  app.post(`${DEFAULT_API}/call/arrive`, async (req, res) => {
+    try {
+      await Call.update(
+        { driverId: req.body.driverId, userId: req.body.userId },
+        { status: CALL_STATUS_ARRIVE }
+      );
+      await User.update(
+        { id: req.body.driverId },
+        { status: USER_STATUS_READY }
+      );
+      await User.update({ id: req.body.userId }, { status: USER_STATUS_READY });
+      await res
+        .status(200)
+        .json({ result: SERVER_RESULT_SUCCESS, msg: "도착!" });
+    } catch (error) {
+      await res
+        .status(401)
+        .json({ result: SERVER_RESULT_FAILURE, msg: "failure arrive" });
+    }
+  });
 
-    // GET call status
-    app.post(`${DEFAULT_API}/call/status`, (req, res) => {
-        if(req.body.type === 'driver') {
-            Call.find({ driverId: req.body.driverId, status: CALL_STATUS_APPROVE }, (err, data) => {
-                if(err) throw err;
-                try {
-                    res.status(200).json({ result: SERVER_RESULT_SUCCESS, msg: "Get status success!", status: data[0].status });
-                } catch (error) {
-                    res.status(401).json({ result: SERVER_RESULT_FAILURE, msg: error });
-                }
+  // GET call status
+  app.post(`${DEFAULT_API}/call/info`, (req, res) => {
+    if (req.body.type === "driver") {
+      Call.find(
+        { driverId: req.body.driverId, status: CALL_STATUS_APPROVE },
+        (err, data) => {
+          if (err) throw err;
+          try {
+            res.status(200).json({
+              result: SERVER_RESULT_SUCCESS,
+              msg: "Get status success!",
+              status: data[0].status,
+              userId: data[0].userId
             });
-        }else if(req.body.type === 'passenger') {
-            Call.find().where('userId').equals(req.body.userId).in('status', [CALL_STATUS_READY, CALL_STATUS_APPROVE]).then(data => {
-                console.log('++++++++++++++++++++++++++++++++++++++++++++++++++++++123', data);
-                try {
-                    res.status(200).json({ result: SERVER_RESULT_SUCCESS, msg: "Get status success!", status: data[0].status });
-                } catch (error) {
-                    res.status(401).json({ result: SERVER_RESULT_FAILURE, msg: error });
-                }
+          } catch (error) {
+            res.status(401).json({ result: SERVER_RESULT_FAILURE, msg: error });
+          }
+        }
+      );
+    } else if (req.body.type === "passenger") {
+      Call.find()
+        .where("userId")
+        .equals(req.body.userId)
+        .in("status", [CALL_STATUS_READY, CALL_STATUS_APPROVE])
+        .then(data => {
+          console.log(
+            "++++++++++++++++++++++++++++++++++++++++++++++++++++++123",
+            data
+          );
+          try {
+            res.status(200).json({
+              result: SERVER_RESULT_SUCCESS,
+              msg: "Get status success!",
+              status: data[0].status,
+              userId: data[0].userId
             });
-        }else {
-            res.status(401).json({ result: SERVER_RESULT_FAILURE, msg: "parameter type error!" });
-        }
-    });
+          } catch (error) {
+            res.status(401).json({ result: SERVER_RESULT_FAILURE, msg: error });
+          }
+        });
+    } else {
+      res
+        .status(401)
+        .json({ result: SERVER_RESULT_FAILURE, msg: "parameter type error!" });
+    }
+  });
 
-    // PUT call approval
-    app.put(`${DEFAULT_API}/call/approval`, async(req, res) => {
-        // driver user status : 0 => 1
-        // passenger user status : 0 => 1
-        // call schema status : 0 => 1
+  // PUT call approval
+  app.put(`${DEFAULT_API}/call/approval`, async (req, res) => {
+    // driver user status : 0 => 1
+    // passenger user status : 0 => 1
+    // call schema status : 0 => 1
+    try {
+      await User.update(
+        { id: req.body.driverId },
+        { status: USER_STATUS_WORK }
+      );
+      await User.update({ id: req.body.userId }, { status: USER_STATUS_WORK });
+      await Call.update(
+        { driverId: req.body.driverId, userId: req.body.userId },
+        { status: CALL_STATUS_APPROVE }
+      );
+      await res
+        .status(200)
+        .json({ result: SERVER_RESULT_SUCCESS, msg: "매칭 완료 HooHoo ~" });
+    } catch (error) {
+      console.log("call/approval : ", error);
+      await res
+        .status(401)
+        .json({ result: SERVER_RESULT_FAILURE, msg: "매칭 실패 FUCK..." });
+    }
+  });
+
+  // PUT call reject
+  app.put(`${DEFAULT_API}/call/reject`, async (req, res) => {
+    // driver user status : 0 => 0
+    // passenger user status : 0 => 0
+    // call schema status : 0 => 2
+    try {
+      await Call.update(
+        { driverId: req.body.driverId, userId: req.body.userId },
+        { status: CALL_STATUS_REJECT }
+      );
+      await res
+        .status(200)
+        .json({ result: SERVER_RESULT_SUCCESS, msg: "거절 완료 HooHoo" });
+    } catch (error) {
+      console.log("call/reject : ", error);
+      await res
+        .status(401)
+        .json({ result: SERVER_RESULT_FAILURE, msg: "거절 실패 FUCK..." });
+    }
+  });
+
+  // GET call list
+  app.post(`${DEFAULT_API}/call/list`, (req, res) => {
+    console.log("calllist req.body.id ===============", req.body.id);
+    Call.find(
+      { driverId: req.body.id, status: CALL_STATUS_READY },
+      (err, calls) => {
+        if (err) throw err;
+        let customCalls = calls.map(call => {
+          return Object.assign({
+            userId: call.userId,
+            sPoint: call.sPoint,
+            destination: call.destination,
+            price: call.price,
+            date: call.call_date
+          });
+        });
+        console.log("call List : ", customCalls);
+
         try {
-            await User.update({ id: req.body.driverId }, { status: USER_STATUS_WORK });
-            await User.update({ id: req.body.userId }, { status: USER_STATUS_WORK });
-            await Call.update({ driverId: req.body.driverId, userId: req.body.userId }, { status: CALL_STATUS_APPROVE });
-            await res
-                .status(200)
-                .json({ result: SERVER_RESULT_SUCCESS, msg: "매칭 완료 HooHoo ~" });
+          res.status(200).json(customCalls);
         } catch (error) {
-            console.log("call/approval : ", error);
-            await res
-                .status(401)
-                .json({ result: SERVER_RESULT_FAILURE, msg: "매칭 실패 FUCK..." });
+          console.log(error);
+          res
+            .status(401)
+            .json({ result: SERVER_RESULT_FAILURE, msg: "다시시도해주세용" });
         }
-    });
+      }
+    );
+  });
 
-    // PUT call reject
-    app.put(`${DEFAULT_API}/call/reject`, async(req, res) => {
-        // driver user status : 0 => 0
-        // passenger user status : 0 => 0
-        // call schema status : 0 => 2
+  // POST Insert call info
+  app.post(`${DEFAULT_API}/call/request`, (req, res) => {
+    console.log(
+      `API: ${DEFAULT_API}/call/request =============== ${req.body} :::::`
+    );
+
+    User.find(
+      { id: req.body.driverId, status: USER_STATUS_READY },
+      (err, data) => {
+        if (err) throw err;
+        // console.log('data =============', data, 'err ======================', err);
+        if (data.length === 1) {
+          Call.find(
+            { driverId: req.body.driverId, userId: req.body.userId },
+            async (err, data) => {
+              if (err) throw err;
+              console.log(
+                "data =============",
+                data,
+                "err ======================",
+                err
+              );
+              const call = new Call({
+                driverId: req.body.driverId,
+                userId: req.body.userId,
+                sPoint: req.body.sPoint,
+                destination: req.body.destination,
+                price: req.body.price,
+                call_date: NOW,
+                status: CALL_STATUS_READY // 0 대기, 1 승인, 2 거절
+              });
+              if (data.length === 1) {
+                console.log("call List update");
+                await Call.update(
+                  { driverId: req.body.driverId, userId: req.body.userId },
+                  {
+                    sPoint: req.body.sPoint,
+                    destination: req.body.destination,
+                    price: req.body.price,
+                    call_date: NOW,
+                    status: CALL_STATUS_READY
+                  }
+                );
+                res
+                  .status(200)
+                  .json({ result: SERVER_RESULT_SUCCESS, msg: "요청갓오용" });
+              } else {
+                //save
+                console.log("call list insert");
+                call.save(err => {
+                  console.log(err);
+                  if (err) {
+                    res.status(401).json({
+                      result: SERVER_RESULT_FAILURE,
+                      msg: "에러낫오용"
+                    });
+                  }
+                  res
+                    .status(200)
+                    .json({ result: SERVER_RESULT_SUCCESS, msg: "요청갓오용" });
+                });
+              }
+            }
+          );
+        } else {
+          res
+            .status(401)
+            .json({ result: SERVER_RESULT_FAILURE, msg: "운전중이에용" });
+        }
+      }
+    );
+  });
+
+  // GET driver info
+  app.get(`${DEFAULT_API}/driver/list`, (req, res) => {
+    User.find(
+      { position: USER_POSITION_DRIVER, status: USER_STATUS_READY },
+      (err, dirvers) => {
+        if (err) throw err;
+        console.log("dirver: ", dirvers);
+        let customDrivers = dirvers.map(driver => {
+          return Object.assign(
+            {},
+            {
+              id: driver.id,
+              name: driver.name,
+              status: driver.status,
+              date: driver.create_date
+            }
+          );
+        });
+        console.log("driver1: ", customDrivers);
         try {
-            await Call.update({ driverId: req.body.driverId, userId: req.body.userId }, { status: CALL_STATUS_REJECT });
-            await res
-                .status(200)
-                .json({ result: SERVER_RESULT_SUCCESS, msg: "거절 완료 HooHoo" });
+          res.status(200).json(customDrivers);
         } catch (error) {
-            console.log("call/reject : ", error);
-            await res
-                .status(401)
-                .json({ result: SERVER_RESULT_FAILURE, msg: "거절 실패 FUCK..." });
+          console.log(error);
         }
-    });
-
-    // GET call list
-    app.post(`${DEFAULT_API}/call/list`, (req, res) => {
-        console.log("calllist req.body.id ===============", req.body.id);
-        Call.find({ driverId: req.body.id, status: CALL_STATUS_READY },
-            (err, calls) => {
-                if (err) throw err;
-                let customCalls = calls.map(call => {
-                    return Object.assign({
-                        userId: call.userId,
-                        sPoint: call.sPoint,
-                        destination: call.destination,
-                        price: call.price,
-                        date: call.call_date
-                    });
-                });
-                console.log("call List : ", customCalls);
-
-                try {
-                    res.status(200).json(customCalls);
-                } catch (error) {
-                    console.log(error);
-                    res
-                        .status(401)
-                        .json({ result: SERVER_RESULT_FAILURE, msg: "다시시도해주세용" });
-                }
-            }
-        );
-    });
-
-    // POST Insert call info
-    app.post(`${DEFAULT_API}/call/request`, (req, res) => {
-        console.log(
-            `API: ${DEFAULT_API}/call/request =============== ${req.body} :::::`
-        );
-
-        User.find({ id: req.body.driverId, status: USER_STATUS_READY },
-            (err, data) => {
-                if (err) throw err;
-                // console.log('data =============', data, 'err ======================', err);
-                if (data.length === 1) {
-                    Call.find({ driverId: req.body.driverId, userId: req.body.userId },
-                        async(err, data) => {
-                            if (err) throw err;
-                            console.log(
-                                "data =============",
-                                data,
-                                "err ======================",
-                                err
-                            );
-                            const call = new Call({
-                                driverId: req.body.driverId,
-                                userId: req.body.userId,
-                                sPoint: req.body.sPoint,
-                                destination: req.body.destination,
-                                price: req.body.price,
-                                call_date: NOW,
-                                status: CALL_STATUS_READY // 0 대기, 1 승인, 2 거절
-                            });
-                            if (data.length === 1) {
-                                console.log("call List update");
-                                await Call.update({ driverId: req.body.driverId, userId: req.body.userId }, {
-                                    sPoint: req.body.sPoint,
-                                    destination: req.body.destination,
-                                    price: req.body.price,
-                                    call_date: NOW,
-                                    status: CALL_STATUS_READY
-                                });
-                                res.status(200).json({ result: SERVER_RESULT_SUCCESS, msg: "요청갓오용" });
-                            } else {
-                                //save
-                                console.log("call list insert");
-                                call.save(err => {
-                                    console.log(err);
-                                    if (err) {
-                                        res.status(401).json({
-                                            result: SERVER_RESULT_FAILURE,
-                                            msg: "에러낫오용"
-                                        });
-                                    }
-                                    res
-                                        .status(200)
-                                        .json({ result: SERVER_RESULT_SUCCESS, msg: "요청갓오용" });
-                                });
-                            }
-                        }
-                    );
-                } else {
-                    res
-                        .status(401)
-                        .json({ result: SERVER_RESULT_FAILURE, msg: "운전중이에용" });
-                }
-            }
-        );
-    });
-
-    // GET driver info
-    app.get(`${DEFAULT_API}/driver/list`, (req, res) => {
-        User.find({ position: USER_POSITION_DRIVER, status: USER_STATUS_READY },
-            (err, dirvers) => {
-                if (err) throw err;
-                console.log("dirver: ", dirvers);
-                let customDrivers = dirvers.map(driver => {
-                    return Object.assign({}, {
-                        id: driver.id,
-                        name: driver.name,
-                        status: driver.status,
-                        date: driver.create_date
-                    });
-                });
-                console.log("driver1: ", customDrivers);
-                try {
-                    res.status(200).json(customDrivers);
-                } catch (error) {
-                    console.log(error);
-                }
-            }
-        );
-    });
+      }
+    );
+  });
 };
 
 export default callRoutes;
--- frontend/src/views/Home/Drive.jsx
+++ frontend/src/views/Home/Drive.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 import Button from "../../components/Button";
 import { CardForm } from "../../components/Card/CardForm";
-import styled, { keyframes } from 'styled-components';
+import styled, { keyframes } from "styled-components";
 
 const Title = styled.div`
   text-align: center;
@@ -15,11 +15,12 @@ const Div = styled.div`
   &:hover {
     background: black;
     color: #fff;
-  };
-  ${props => props.userID === props.id ?
-    `background: black;
+  }
+  ${props =>
+    props.userID === props.id
+      ? `background: black;
     color: #fff;`
-  : null}
+      : null}
 `;
 
 const swing = keyframes`
@@ -40,83 +41,112 @@ const passengerSwing = keyframes`
 `;
 
 const Swing = styled.div`
-    position: fixed;
-    animation: ${props => props.type === 'driver' ? swing : passengerSwing} 2s linear infinite;
+  position: fixed;
+  top: 60%;
+  animation: ${props => (props.type === "driver" ? swing : passengerSwing)} 2s
+    linear infinite;
 `;
 
-const Drive = (props) => {
-    const { userInfo, userID, drivers, calls, handleSelectUser, handleClick, handleReject, isPassengerHome, isDriverHome } = props;
-    console.log({isPassengerHome}, {isDriverHome});
+const Drive = props => {
+  const {
+    userInfo,
+    userID,
+    drivers,
+    calls,
+    handleSelectUser,
+    handleClick,
+    handleReject,
+    isPassengerHome,
+    isDriverHome,
+    handleArrive
+  } = props;
+  console.log({ isPassengerHome }, { isDriverHome });
 
-    const driverElem = drivers.map(el => {
-        return(el.status === 0 ?
-          <CardForm key={el.id} >
-            <Div onClick={handleSelectUser(el.id)} userID={userID} id={el.id}>
-              <h2>{el.id}</h2>
-              <span>{el.name}</span>
-              <span>{el.date}</span><br/>
-            </Div>
-          </CardForm>
-          : null);
-      });
+  const driverElem = drivers.map(el => {
+    return el.status === 0 ? (
+      <CardForm key={el.id}>
+        <Div onClick={handleSelectUser(el.id)} userID={userID} id={el.id}>
+          <h2>{el.id}</h2>
+          <span>{el.name}</span>
+          <span>{el.date}</span>
+          <br />
+        </Div>
+      </CardForm>
+    ) : null;
+  });
 
-      const callElem = calls.map((el, idx) => {
-        if(el.sPoint === '') {
-          el.sPoint = '여기서부터 ';
-        }
-        if(el.destination === '') {
-          el.destination = '저기까지';
-        }
-        return (
-          <CardForm key={idx}>
-            <Div onClick={handleSelectUser(el.userId)} userID={userID} id={el.userId}>
-              <h2>{el.userId}</h2>
-              <p>{`${el.sPoint} ~ ${el.destination}`}</p>
-              <p>{el.price <= 0 ? '꽁짜' : el.price}</p>
-              <p>{el.date}</p>
-            </Div>
-          </CardForm>
-        )
-      });
-
-    return(!isPassengerHome && !isDriverHome ?
-        <div>
-            {!userInfo.position || userInfo.position === 0 ? (
-              <div>
-                <Title>마음에 드는 운전자를 선택하세요:D</Title>
-                {driverElem}
-                <CardForm>
-                  <Button onClick={handleClick}>
-                    call
-                  </Button>
-                </CardForm>
-              </div>
-              ) : (
-              <div>
-                <Title>당신이 요청받은 콜 리스트 입니다:D</Title>
-                {callElem}
-                <Button onClick={handleClick} style={{width:'45%', margin:'auto 2.5%'}}>
-                  Ok
-                </Button>
-                <Button onClick={handleReject} style={{width:'45%', margin:'auto 2.5%'}}>
-                  No
-                </Button>
-              </div>
-            )}
-        </div>
-        :
-        <div>
-          {!userInfo.position || userInfo.position === 0 ? (
-            <Swing type="driver">
-              <img src="/mungCar.png" alt="멍카" />
-            </Swing>
-            ) : (
-            <Swing type="passenger">
-              <img src="/qvil.png" alt="태수" />
-            </Swing>
-          )}
-        </div>
+  const callElem = calls.map((el, idx) => {
+    if (el.sPoint === "") {
+      el.sPoint = "여기서부터 ";
+    }
+    if (el.destination === "") {
+      el.destination = "저기까지";
+    }
+    return (
+      <CardForm key={idx}>
+        <Div
+          onClick={handleSelectUser(el.userId)}
+          userID={userID}
+          id={el.userId}
+        >
+          <h2>{el.userId}</h2>
+          <p>{`${el.sPoint} ~ ${el.destination}`}</p>
+          <p>{el.price <= 0 ? "꽁짜" : el.price}</p>
+          <p>{el.date}</p>
+        </Div>
+      </CardForm>
     );
-}
+  });
+
+  return !isPassengerHome && !isDriverHome ? (
+    <div>
+      {!userInfo.position || userInfo.position === 0 ? (
+        <div>
+          <Title>마음에 드는 운전자를 선택하세요:D</Title>
+          {driverElem}
+          <CardForm>
+            <Button onClick={handleClick}>call</Button>
+          </CardForm>
+        </div>
+      ) : (
+        <div>
+          <Title>당신이 요청받은 콜 리스트 입니다:D</Title>
+          {callElem}
+          <Button
+            onClick={handleClick}
+            style={{ width: "45%", margin: "auto 2.5%" }}
+          >
+            Ok
+          </Button>
+          <Button
+            onClick={handleReject}
+            style={{ width: "45%", margin: "auto 2.5%" }}
+          >
+            No
+          </Button>
+        </div>
+      )}
+    </div>
+  ) : (
+    <div>
+      {!userInfo.position || userInfo.position === 0 ? (
+        <Swing type="driver">
+          <img src="/mungCar.png" alt="멍카" />
+        </Swing>
+      ) : (
+        <div>
+          <Swing type="passenger">
+            <img src="/qvil.png" alt="태수" />
+          </Swing>
+          <CardForm
+            style={{ position: "absolute", bottom: "10%", width: "90%" }}
+          >
+            <Button onClick={handleArrive}>도착</Button>
+          </CardForm>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default Drive;
