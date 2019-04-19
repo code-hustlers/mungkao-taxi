835a6aba62253e41763d77876196bcc929f8f8db
kang <kaspi6000@gmail.com>
2019-04-19 18:47:05 +0900

    overview ui 수정

--- frontend/src/views/Home/OverView.jsx
+++ frontend/src/views/Home/OverView.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Div = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+const DownIcon = styled.img.attrs({src: '/expand.png'})`
+    position: absolute;
+    top: 90%;
+    width: 1rem;
+    height: 1rem;
+`;
 
 const OverView = (props) => {
     const { userInfo, fcmTest } = props;
 
     return(
-        <div>
-            <span style={{ color: "#8e44ad" }}>{userInfo.id}</span>
-            <span>님, 어서오세요.</span>
+        <Div>
+            <span style={{ color: "#8e44ad" }}>{userInfo.id} <span style={{color:'#555'}}>님, 어서오세요.</span></span>
             <button onClick={fcmTest}>FCM Test</button>
-        </div>
+            <a href="#drive">
+                <DownIcon />
+            </a>
+        </Div>
     );
 }
 
